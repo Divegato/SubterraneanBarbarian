f913722b761e993126b71026f9c2a7061771cbf5
Brandon Salmon <brandon.j.salmon@gmail.com>
2014-09-14 23:05:03 -0500

    Hexagons!

--- tests/spec/GameServerSpec.js
+++ tests/spec/GameServerSpec.js
@@ -17,5 +17,39 @@ describe("Game Server", function () {
             expect(io.on).toHaveBeenCalledWith('connection', jasmine.any(Function));
         });
 
+        describe("onConnection", function () {
+            var socket;
+
+            beforeEach(function () {
+                socket = { on: jasmine.createSpy('socket.on') };
+                gameFunction.registerPlayer = jasmine.createSpy('registerPlayer');
+            });
+
+            it("registers a new player", function () {
+                gameFunction.onConnection(io, socket);
+
+                expect(gameFunction.registerPlayer).toHaveBeenCalled();
+            });
+
+            it("registers disconnect event", function () {
+                gameFunction.onConnection(io, socket);
+
+                expect(socket.on).toHaveBeenCalledWith('disconnect', jasmine.any(Function));
+            });
+
+            it("registers look event", function () {
+                gameFunction.onConnection(io, socket);
+
+                expect(socket.on).toHaveBeenCalledWith('look', jasmine.any(Function));
+            });
+
+            it("registers move event", function () {
+                gameFunction.onConnection(io, socket);
+
+                expect(socket.on).toHaveBeenCalledWith('move', jasmine.any(Function));
+            });
+
+        });
+
     });
 });
